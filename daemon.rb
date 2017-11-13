fab472359f95bf40aa40b21a156692e611ff141a
Kenneth Miller <kmiller@emtrain.com>
2017-11-13 11:34:36 -0800

    updated daemon script to use rbenv sudo

--- daemon.rb
+++ daemon.rb
@@ -4,5 +4,5 @@ require 'daemons'
 pwd = Dir.pwd
 Daemons.run_proc('kechimyaku.rb', {:dir_mode => :normal}) do
   Dir.chdir(pwd)
-  exec "ruby kechimyaku.rb"
+  exec "rbenv sudo ruby kechimyaku.rb -o 172.26.2.193 -p 80"
 end
